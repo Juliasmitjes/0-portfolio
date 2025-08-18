250030ee377ecda721312a285dd7a273f1ad5a04
lilJ <julia.s@live.nl>
2025-08-19 00:16:00 +0200

    add resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,8 +2,36 @@ import React from 'react';
 
 const Resume = () => {
   return (
-    <div className="bg-white py-20 text-center w-screen">
-      <h1> resume </h1>
+    <div className="flex flex-row bg-white py-20 text-center w-screen">
+      <div className="flex flex-col justify-center w-1/2 pl-20">
+        <h1 className="mb-3 text-4xl font-myHeader text-myBlue"> Education </h1>
+        <p className="text-2xl font-myText text-myOcean"> MSc with econometrics 
+          <br />
+          Utrecht University
+          <br />
+          Awarded Cum Laude
+        </p>
+  
+        <h1 className="mt-10 mb-3 text-4xl font-myHeader text-myBlue"> Relevant experience </h1>
+        <p className="text-2xl font-myText text-myOcean"> 3+ years web design 
+          <br />
+          Programming training
+        </p>
+
+
+      </div> 
+      <div className="flex flex-col justify-center w-1/2 pr-20">
+        <h1 className="mb-3 text-4xl font-myHeader text-myBlue"> Skills and interest </h1>
+        <p className="text-2xl font-myText text-myOcean font-semibold"> Frontend webdevelopment 
+        </p>
+         <p className="text-2xl font-myText text-myOcean">
+          HTML, (Taiwlind) CSS, JavaScript, React + Vite
+       
+         <p className="text-2xl font-myText text-myOcean font-semibold pt-3"> Frontend webdevelopment 
+        </p>
+          Node.js, Express.js, Git, PostgreSQL
+        </p>
+      </div> 
     </div>
   );
 };

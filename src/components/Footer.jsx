4104190077a471685494f91ea3bb7beb56d6323f
lilJ <julia.s@live.nl>
2025-08-21 12:39:12 +0200

    add link footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,32 @@
 
 import { FaGithub, FaPhoneAlt } from "react-icons/fa";
 import { MdAlternateEmail } from "react-icons/md";
+import { HashLink } from 'react-router-hash-link';
+
+const navLinks = [
+  { name: 'Julia Smitjes', href: '/#aboutme' },
+  { name: '', href: '#' },
+  { name: '', href: '#' },  
+]
+
 
 const Footer = () => {
   return (
-    <div className="bg-myDark pb-6">
+<div className="bg-myDark pb-6">
     <footer className="bg-myWarm md:flex md:items-center md:justify-between p-10 pt-20 mx-6">
         <ul className="flex items-center flex-wrap mb-6 md:mb-0">
-            <li><a href="#" className="text-myDark font-myText hover:text-myBlue text-lg ml-10 mr-4 md:mr-6">Julia Smitjes</a></li>
+            {navLinks.map(link => (
+            <>
+            <li key={link.name}>
+            <HashLink
+                to={link.href}
+                className="text-myDark font-myText hover:text-myBlue text-lg ml-10 mr-4 md:mr-6"
+            >
+                {link.name}
+            </HashLink>
+            </li>
+            </>
+            ))}
         </ul>
         <div className="flex sm:justify-center space-x-6 mr-10">
             <a href="#" className="text-myDark font-myText hover:text-myBlue">

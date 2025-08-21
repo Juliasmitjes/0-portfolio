dc9c3000912dcbcc9c0194cfdb7593c41f5edad0
lilJ <julia.s@live.nl>
2025-08-21 08:54:35 +0200

    endmessage tictactoe

--- src/components/games/tictactoe/Tictactoe.jsx
+++ src/components/games/tictactoe/Tictactoe.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { GiPodiumWinner } from "react-icons/gi";
+import { FaTimes } from "react-icons/fa";
 
 function Square({ value, onSquareClick }) {
   return (
@@ -8,7 +10,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function Board({ xIsNext, squares, onPlay }) {
+function Board({ xIsNext, squares, onPlay, endMessage, setEndMessage }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -22,7 +24,7 @@ function Board({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares);
   }
 
-  const winner = calculateWinner(squares);
+  const winner = calculateWinner(squares, setEndMessage);
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
@@ -30,8 +32,42 @@ function Board({ xIsNext, squares, onPlay }) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
+  
+
   return (
     <>
+    {endMessage && (
+          <div
+            className="absolute inset-0 flex items-center justify-center z-20"
+            role="dialog"
+            aria-modal="true"
+          >
+            <div className="relative bg-white p-6 rounded-lg text-center shadow-lg">
+              <button
+              type="button"
+              aria-label="Close"
+              className="absolute top-2 right-2 text-myBlue cursor-pointer hover:text-myDark text-3xl"
+              onClick={() => setEndMessage("")}
+                >
+              <span className="sr-only">Close</span>
+              <FaTimes size={20} />
+              </button>
+    
+              <GiPodiumWinner className="mx-auto mb-2 text-5xl font-myText text-myBlue" />
+              <p className="mb-6 text-lg font-myText text-myOcean">{endMessage}</p>
+              <button
+                className="px-4 py-2 bg-myBlue font-myText font-bold text-white rounded hover:bg-myDark cursor-pointer"
+                onClick={() => {
+                  setEndMessage("");         
+                  window.location.reload();   
+                }}
+              >
+                Play again
+              </button>
+            </div>
+          </div>
+        )}
+        
      <div className="my-8 flex items-center justify-center flex-col">
        <div className="pb-8">{status}</div>
       <div className="grid grid-cols-3 w-xs h-[100px]">
@@ -56,6 +92,7 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 const Tictactoe = () => {
+  const [endMessage, setEndMessage] = useState("");
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
@@ -66,21 +103,24 @@ const Tictactoe = () => {
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
-
-  
-
   return (
     <div className="bg-myDark">
       <div className="bg-myWarm mx-6 flex items-center justify-center flex-col">
     <div className="text-4xl font-myHeader text-myBlue">
-      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      <Board 
+      xIsNext={xIsNext} 
+      squares={currentSquares} 
+      onPlay={handlePlay}
+      endMessage={endMessage}
+      setEndMessage={setEndMessage}
+     />
     </div>
   </div>
 </div>
   );
 }
 
-function calculateWinner(squares) {
+function calculateWinner(squares, setEndMessage) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -91,9 +131,11 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      setEndMessage(`The winner is player ${squares[a]}. Would you like to play again?`);
       return squares[a];
     }
   }

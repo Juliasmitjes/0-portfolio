28bdf64a401ca6646e67e97b7ffcea908aaa4cd6
lilJ <julia.s@live.nl>
2025-08-22 13:18:02 +0200

    delete home image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,55 +18,44 @@ const Home = () => {
       <p className="text-3xl font-myText font-bold text-myOcean pt-4 pb-20">  
         Running on Wi-Fi, espresso, and curiosity.
       </p>
-
-    <div className="relative inline-block">
-        <img className="h-100 object-scale-down rounded-lg " src="images/JuliaSmitjesImage.jpeg" alt="Julia Smitjes" />
-        <div className="absolute bottom-0 -left-15 w-60 h-115">
-          <img src="images/pen1.png" alt="blue"
-              className="absolute top-6 left-6 z-0" />
+      <img className="h-100 object-scale-down rounded-lg " src="images/JuliaSmitjesImage.jpeg" alt="Julia Smitjes" />
+      </div>   
+        <div className="flex flex-col place-items-center justify-center bg-myWarm pt-15 mx-4">
+          <h1 className="text-4xl font-myHeader text-myBlue pb-8">
+            A bit about me
+          </h1>
+          <p className="text-2xl font-bold font-myText text-myOcean pb-25 max-w-lg text-center">
+            I light up around good ideas and great people. From debugging code to designing systems, I’m all about mixing tech with taste.
+          </p>
+          </div>
+        <div className="flex flex-col items-center bg-myOcean">
+          <div id="resume-section" className="w-2xl h-96 mb-54 scroll-mt-19">
+            <Resume />
+          </div>
         </div>
-        <div className="absolute bottom-0 -right-10 w-60 h-38">
-          <img src="images/pen2.png" alt="blue"
-            className="absolute top-0 left-0 z-10 rotate-[20deg]" />
-      </div>
+        
+        <div id="projects-section" >
+          <Projects />
+        </div>
+          <div>
+            <div className="flex flex-col place-items-center justify-center bg-myOcean pt-15">
+            <h1 className="text-4xl font-myHeader text-myBlue pb-8">
+              Contact
+            </h1>
+            <p className="text-2xl font-myText text-white font-bold pb-8 max-w-lg text-center">
+              Let’s talk. I’d love to hear from you.
+            </p>
+            <button
+                type="button"
+                onClick={() => setShowContactForm(true)}
+                className="hidden md:inline-flex items-center font-myText px-4 py-2 bg-myBlue text-white rounded-lg text-lg cursor-pointer font-bold hover:bg-myDark focus:outline-none focus:ring-2 focus:ring-blue-300 mb-6"
+              >
+                Get in touch
+          </button>
+          </div>
+        </div>
+      </div>  
     </div>
-  </div>   
-    <div className="flex flex-col place-items-center justify-center bg-myWarm pt-15 mx-4">
-      <h1 className="text-4xl font-myHeader text-myBlue pb-8">
-        A bit about me
-      </h1>
-      <p className="text-2xl font-bold font-myText text-myOcean pb-25 max-w-lg text-center">
-        I light up around good ideas and great people. From debugging code to designing systems, I’m all about mixing tech with taste.
-      </p>
-      </div>
-    <div className="flex flex-col items-center bg-myBlue">
-      <div id="resume-section" className="w-2xl h-96 mb-54 scroll-mt-19">
-        <Resume />
-      </div>
-    </div>
-    
-    <div id="projects-section" >
-      <Projects />
-    </div>
-      <div>
-        <div className="flex flex-col place-items-center justify-center bg-myOcean pt-20">
-        <h1 className="text-4xl font-myHeader text-myBlue pb-8">
-          Contact
-        </h1>
-        <p className="text-2xl font-myText text-white font-bold pb-8 max-w-lg text-center">
-          Let’s talk. I’d love to hear from you.
-        </p>
-        <button
-            type="button"
-            onClick={() => setShowContactForm(true)}
-            className="hidden md:inline-flex items-center font-myText px-4 py-2 bg-myBlue text-white rounded-lg text-lg cursor-pointer font-bold hover:bg-myDark focus:outline-none focus:ring-2 focus:ring-blue-300 mb-6"
-          >
-            Get in touch
-       </button>
-      </div>
-    </div>
-  </div>  
-</div>
   )
 }
 
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -7,7 +7,7 @@ const Resume = () => {
      <a
         href="/path/to/resume.pdf"
         download
-        className="md:inline-flex items-center font-myText px-4 py-2 bg-myOcean text-white rounded-lg text-lg cursor-pointer font-bold hover:bg-myDark mt-20"
+        className="md:inline-flex items-center font-myText px-4 py-2 bg-myBlue text-white rounded-lg text-lg cursor-pointer font-bold hover:bg-myDark mt-20"
       >
       Download resume
       </a>

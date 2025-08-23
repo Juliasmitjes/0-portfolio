f1ce411f1848bbc1e21953e75422d1ee22d73ffc
lilJ <julia.s@live.nl>
2025-08-23 15:09:13 +0200

    add Resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,7 +5,7 @@ const Resume = () => {
   <div className="sm:pt-10 flex flex-col items-center">
     <TextCarousel />
      <a
-        href="/path/to/resume.pdf"
+        href={`${import.meta.env.BASE_URL}ResumeJuliaSmitjes.pdf`}
         download
         className="block w-full text-center sm:w-auto md:inline-flex items-center font-myText sm:mt-10 px-4 py-2 bg-myBlue text-white sm:rounded-lg text-lg cursor-pointer font-bold hover:bg-myDark active:scale-110 sm:active:scale-100"
       >
